05ceb4614160d38f36e5c0739c0830288f029bf6
Matthew <mwilliams279@toromail.csudh.edu>
2018-01-10 21:29:08 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,17 @@ function getLocation() {
 function showPosition(position) {
     var coordinate = position.coords.latitude + ", " + position.coords.longitude;
   alert(coordinate);
-  return coordinate;
+function initMap() {
+        var aproxLoc = coordinate; 
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: aproxLoc
+        });
+        var marker = new google.maps.Marker({
+          position: aproxLoc,
+          map: map
+        });
+      }
 }
 
 var loc = showPosition();
@@ -22,14 +32,3 @@ document.getElementById("demo").innerHTML = loc;
 
 
 
-function initMap() {
-        var aproxLoc = coordinates; 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: aproxLoc
-        });
-        var marker = new google.maps.Marker({
-          position: aproxLoc,
-          map: map
-        });
-      }
