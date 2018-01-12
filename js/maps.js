9a076284d64b14d82dc9d6a390b6b651279c0e92
Matthew <mwilliams279@toromail.csudh.edu>
2018-01-12 15:39:00 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,16 +1,18 @@
 var map, infoWindow;
       function initMap() {
+              var myLoc = new google.maps.LatLng(33.7836946323483,-118.13152550663756);
+
             /*
             google.maps.Map() takes 2 parameters separated by a comma: 
             1. the location of the map on your page, and 
             2. the details/options of the map in the form of object properties. 
             */
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 33.7836946323483, lng: -118.13152550663756},
+          center: {lat: 33.770050, lng: -118.193739},
           zoom: 11
         });
         infoWindow = new google.maps.InfoWindow;
-            infoWindow.setPosition(map.center);       
+            infoWindow.setPosition({position:myLoc});       
             //originally infoWindow.setPosition(pos);
             infoWindow.setContent('I am here.');
             infoWindow.open(map);
