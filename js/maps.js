4a838e3f1c97958582456b4e5aedf7afc43aa1e0
Matthew <mwilliams279@toromail.csudh.edu>
2018-01-12 14:09:32 -0800

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@ var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 33.7836946323483, lng: -118.13152550663756},
-          zoom: 24
+          zoom: 11
         });
         infoWindow = new google.maps.InfoWindow;
       }
